8402a60cfc5855b81e48944b7f243e970011a6ae
Oryna Kubarieva <arina.ak47@gmail.com>
2024-09-16 23:01:57 -0400

    correcting the names of the files

---
+++ project1/1-node-farm/starter/secondPart.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+fs.readFile('./txt/start.txt','utf-8',  (err, data1) => {
+	if(err) return console.log("ERROR 🤯💣");
+		// to open emogis panel control + commande + space
+
+	fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+		console.log(data2);
+	
+		fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+			console.log(data3);
+	
+			fs.writeFile('./txt/final.txt', `${data2}\n${data3}`,  'utf-8', err => {
+			console.log('Your file has been writen =) ');
+			})
+		});	
+	});
+});
+
+console.log('Will read File!');
+
+
+
+//Node.js will read the file and will continuer the execution anyways , so it's
+// not going to block code after the second line
