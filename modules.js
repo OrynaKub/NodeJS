81c6a8a7995a01a67648ca60b157860f7c532620
Oryna Kubarieva <arina.ak47@gmail.com>
2025-07-11 17:33:29 -0400

    testing with another methode

--- modules.js
+++ modules.js
@@ -7,5 +7,8 @@ const calc1 = new C();
 console.log(calc1.add(2, 5));
 
 //exports
-const cacl2 = require('./test-module-2'); // calc2 - exports Object
-console.log(cacl2.divine(2, 5));
+// const cacl2 = require('./test-module-2'); // calc2 - exports Object
+const { add, multiply } = require('./test-module-2');
+//console.log(cacl2.multiply(2, 5));
+
+console.log(multiply(2, 5));
