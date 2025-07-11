e4ef6666c29207c57ec6944fca046b01e996a37c
Oryna Kubarieva <arina.ak47@gmail.com>
2025-07-11 17:27:21 -0400

    adding some functions

--- modules.js
+++ modules.js
@@ -1,1 +1,6 @@
-console.log(arguments);
+//console.log(arguments);
+//console.log(require('modules').wrapper);
+
+const C = require('./test-module-1'); // for class  - using a uppercase
+const calc1 = new C();
+console.log(calc1.add(2, 5));
