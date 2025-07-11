acf701292f0bee64b2c937935a05851babc88b8f
Oryna Kubarieva <arina.ak47@gmail.com>
2025-07-11 17:30:34 -0400

    adding test-module-2.js and trying exports this way

--- modules.js
+++ modules.js
@@ -1,6 +1,11 @@
 //console.log(arguments);
 //console.log(require('modules').wrapper);
 
+//module.exports
 const C = require('./test-module-1'); // for class  - using a uppercase
 const calc1 = new C();
 console.log(calc1.add(2, 5));
+
+//exports
+const cacl2 = require('./test-module-2'); // calc2 - exports Object
+console.log(cacl2.divine(2, 5));
