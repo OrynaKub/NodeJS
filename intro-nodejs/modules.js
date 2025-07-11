ce0fe35acf09c853a62fcf7276fdd3220208a90e
Oryna Kubarieva <arina.ak47@gmail.com>
2025-07-11 17:43:14 -0400

    moving everything to new folder

---
+++ intro-nodejs/modules.js
@@ -0,0 +1,19 @@
+//console.log(arguments);
+//console.log(require('modules').wrapper);
+
+//module.exports
+const C = require('./test-module-1'); // for class  - using a uppercase
+const calc1 = new C();
+console.log(calc1.add(2, 5));
+
+//exports
+// const cacl2 = require('./test-module-2'); // calc2 - exports Object
+const { add, multiply } = require('./test-module-2');
+//console.log(cacl2.multiply(2, 5));
+
+console.log(multiply(2, 5));
+
+//caching
+require('./test-module-3')();
+require('./test-module-3')();
+require('./test-module-3')();
