351207049069aff2680960029715c9aa0bc31c81
Oryna Kubarieva <arina.ak47@gmail.com>
2025-07-11 16:17:20 -0400

    adding solution #2

--- streams.js
+++ streams.js
@@ -3,9 +3,22 @@ const server = require('http').createServer();
 
 server.on('request', (req, res) => {
   //Solution 1 - entire file was load to the memory (problem)
-  fs.readFileSync('test-file.txt', (err, data) => {
-    if (err) console.log(err);
-    res.end(data);
+  //   fs.readFileSync('test-file.txt', (err, data) => {
+  //     if (err) console.log(err);
+  //     res.end(data);
+  //   });
+  //Solution 2 : streams
+  const readable = fs.createReadStream('test-file.txt');
+  readable.on('data', (chunk) => {
+    res.write(chunk);
+  });
+  readable.on('end', () => {
+    res.end();
+  });
+  readable.errored('error', (err) => {
+    console.log(err);
+    res.statusCode = 500;
+    res.end('File not found!');
   });
 });
 
