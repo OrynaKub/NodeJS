362a98e156516d12d77226e9a9e53ed2d5e0a71b
Kubarieva,Oryna <ed791977@ens.uqam.ca>
2025-07-12 20:04:51 -0400

    starting API:Handling GET Requests

--- natours/app.js
+++ natours/app.js
@@ -1,16 +1,33 @@
+const fs = require('fs')
 const express = require('express');
 
 const app = express();
 
 //http methode for the request, get send to server for this url : "/"
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
   res
     .status(200)
     .json({ message: 'Hello from the server side!', app: 'Natours' });
-});
+}); 
 
-app.post('/', (req, res) => {
+ app.post('/', (req, res) => {
   res.send('You can post to this endpoint...');
+});
+ */
+
+//read info before writing anything
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+//always specifia the version of the API
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = 3000;
